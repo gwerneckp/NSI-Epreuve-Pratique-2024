f020558934fa30f3d58044fd6f74f829afd71096
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-04-17 20:42:03 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Exercice test de fonctionnement
+def hello_world():
+    return  "Hello"
+
+#Ci-dessous le test réaliser pour valider votre travaillll
+"""
+def test_hello():
+    assert hello.hello_world() == "Hello World!"
+"""
