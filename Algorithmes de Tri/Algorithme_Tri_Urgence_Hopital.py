72d795dd3ba91db2efbb8ec706a2184967c3ecff
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-04-18 15:21:49 +0000

    Hopital sujet

--- Algorithmes de Tri/Algorithme_Tri_Urgence_Hopital.py
+++ Algorithmes de Tri/Algorithme_Tri_Urgence_Hopital.py
@@ -1,1 +1,39 @@
-# Fonction tri
+# Fonction tri selection
+
+def tri(attente):
+    for i in range(len(attente)):
+        pos = i
+        mini = attente[i][1]
+        for j in range(i, len(attente)):
+            if attente[j][1] < mini:
+                pos = j
+                mini = attente[j][1]
+        temp = attente[i]
+        attente[i] = attente[pos]
+        attente[pos] = temp
+        return None
+
+
+def quitte(attente, nom):
+    return [...]
+
+def maj(attente):
+    ...
+    return ...
+
+def priorite(attente, p):
+    ...
+    return ...
+
+
+def revise(attente, p):
+    nouvelle = []
+    n = priorite(attente, p)
+    for (patient, prio) in attente:
+        if patient == p:
+            nouvelle.append(...)
+        elif ...:
+            nouvelle.append((patient, prio + 1))
+        else:
+            nouvelle.append((patient, prio))
+    return nouvelle
---
+++ Algorithmes de Tri/Algorithme_Tri_Urgence_Hopital_test.py
@@ -0,0 +1,20 @@
+import Algorithme_Tri_Urgence_Hopital as algo;
+
+def test_algo():
+    assert algo.tri(attente) == "Hello World!"
+
+
+def test_quitte():
+    assert algo.quitte(attente, nom) == "Hello World!"
+
+def test_maj():
+    assert algo.maj(attente) == "Hello World!"
+
+def test_priorite():
+    assert algo.priorite(attente, p) == "Hello World!"
+
+
+def test_revise():
+    assert algo.revise(attente, p) == "Hello World!"
+
+
