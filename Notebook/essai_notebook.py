a75e4681508a64c3aa263b5d3b84ae026d954703
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-04-21 21:53:01 +0000

    bash conv final

---
+++ Notebook/essai_notebook.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import math
+
+print("Enter the radius of the circle: ")
+
+
+# # exercice entrainement
+# 
+# Finir cette fonction
+# 
+# ![Graphe](290g.png)
+# 
+
+# In[ ]:
+
+
+def fonctions(r):
+    area = math.pi * r**5
+    return area
+
--- test_bash_conv.sh
+++ test_bash_conv.sh
@@ -12,14 +12,9 @@ chemin_notebook="${repertoire_notebook}/${nom_fichier_notebook}"
 # Chemin vers le fichier Python généré
 chemin_python="${repertoire_notebook}/${nom_fichier_notebook%.*}.py"
 
-# Vérifie si le fichier Python existe déjà
-if [ -f "$chemin_python" ]; then
-    # Si oui, le supprime
-    rm "$chemin_python"
-fi
 
 # Conversion du notebook en script Python
-jupyter nbconvert --to script "${chemin_notebook}" --output "${chemin_python}" --execute
+jupyter nbconvert --to script "${chemin_notebook}" 
 
 # Exécution du test unitaire sur le script Python généré
 pytest "${repertoire_notebook}/essai_notebook_test.py"
