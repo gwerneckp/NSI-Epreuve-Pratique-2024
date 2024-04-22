631369c14b0f72f47a14f12e36159d0b34be2489
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-04-22 18:10:53 +0100

    Update test_bash_conv.sh

--- test_bash_conv.sh
+++ test_bash_conv.sh
@@ -10,13 +10,14 @@ nom_fichier_notebook="essai_notebook.ipynb"
 repertoire_destination="."
 
 # Chemin vers le fichier notebook
-chemin_notebook="${repertoire_destination}/"${repertoire_notebook}/${nom_fichier_notebook}"
+chemin_notebook="${repertoire_destination}/${repertoire_notebook}/${nom_fichier_notebook}"
 
 # Chemin vers le fichier Python généré
-chemin_python="${repertoire_destination}/"${repertoire_notebook}/${nom_fichier_notebook%.*}.py"
+chemin_python="${repertoire_destination}/${repertoire_notebook}/${nom_fichier_notebook%.*}.py"
+
 
 # Conversion du notebook en script Python
 jupyter nbconvert --to script "${chemin_notebook}" 
 
 # Exécution du test unitaire sur le script Python généré
-pytest "${repertoire_destination}/"${repertoire_notebook}/essai_notebook_test.py"
+pytest "${repertoire_destination}/${repertoire_notebook}/essai_notebook_test.py"
