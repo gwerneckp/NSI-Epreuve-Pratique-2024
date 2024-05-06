6b3b9c63541b803039ad0ff99773418c27668fd2
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-05-06 15:11:20 +0000

    corrrection

--- Epreuve_Pratique/ep_sujets_test.py
+++ Epreuve_Pratique/ep_sujets_test.py
@@ -4,7 +4,7 @@ import ep_sujets
 
 class TestEpSujets(unittest.TestCase):
 
-    def test_taille_1.1(self):
+    def test_taille_1_1(self):
         a = {'F':['B','G'], 'B':['A','D'], 'A':['',''], 'D':['C','E'], \
             'C':['',''], 'E':['',''], 'G':['','I'], 'I':['','H'], \
             'H':['','']}
@@ -12,7 +12,7 @@ class TestEpSujets(unittest.TestCase):
         self.assertEqual(ep_sujets.taille(a, 'B'),5)
         self.assertEqual(ep_sujets.taille(a, 'I'),2)
     
-    def test_tri_selection_1.2(self):
+    def test_tri_selection_1_2(self):
         self.assertEqual(ep_sujets.tri_selection([41, 55, 21, 18, 12, 6, 25]), [6, 12, 18, 21, 25, 41, 55])
         self.assertEqual(ep_sujets.tri_selection([5, 3, 4, 1, 2]), [1, 2, 3, 4, 5])
         self.assertEqual(ep_sujets.tri_selection([]), [])
