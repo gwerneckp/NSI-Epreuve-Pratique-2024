f8e22d121474bcd40ffb114006ec40ff847355e6
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-05-06 11:09:59 +0000

    correction

--- Epreuve_Pratique/ep_sujets_test.py
+++ Epreuve_Pratique/ep_sujets_test.py
@@ -5,6 +5,9 @@ import ep_sujets
 class TestEpSujets(unittest.TestCase):
 
     def test_taille(self):
+        a = {'F':['B','G'], 'B':['A','D'], 'A':['',''], 'D':['C','E'], \
+'C':['',''], 'E':['',''], 'G':['','I'], 'I':['','H'], \
+'H':['','']}
         self.assertEqual(ep_sujets.taille(a, 'F')==9)
         self.assertEqual(ep_sujets.taille(a, 'B')==5)
         self.assertEqual(ep_sujets.taille(a, 'I')==2)
