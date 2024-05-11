0f328bdcf72e29f6b6a47aecb554a146c9ec0268
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-11 15:47:27 +0100

    Sujet 01 completed

--- sujets/sujet_01/sujet_answer_01.py
+++ sujets/sujet_01/sujet_answer_01.py
@@ -1,3 +1,5 @@
+from unittest import TestCase, main
+
 a = {
     "F": ["B", "G"],
     "B": ["A", "D"],
@@ -12,25 +14,55 @@ a = {
 
 
 # Votre code
-def taille(arbre, lettre):
-    pass
+def taille(arbre: dict[list[str]], lettre: str):
+    if not arbre[lettre][0] and not arbre[lettre][1]:
+        return 1
+    if not arbre[lettre][0]:
+        return 1 + taille(arbre, arbre[lettre][1])
+    if not arbre[lettre][1]:
+        return 1 + taille(arbre, arbre[lettre][0])
+    return 1 + taille(arbre, arbre[lettre][0]) + taille(arbre, arbre[lettre][1])
 
 
-def echange(tab, i, j):
+def echange(tab: list, i: int, j: int) -> None:
     """Echange les éléments d'indice i et j dans le tableau tab."""
-    temp = ...
-    tab[i] = ...
-    tab[j] = ...
+    # temp = tab[i]
+    # tab[i] = tab[j]
+    # tab[j] = temp
+    tab[i], tab[j] = tab[j], tab[i]
 
 
-def tri_selection(tab):
+def tri_selection(tab: list[int]) -> list[int]:
     """Trie le tableau tab dans l'ordre croissant
     par la méthode du tri par sélection."""
     N = len(tab)
-    for k in range(...):
-        imin = ...
-        for i in range(..., N):
-            if tab[i] < ...:
+    for k in range(N):
+        imin = k
+        for i in range(k, N):
+            if tab[i] < tab[imin]:
                 imin = i
-        echange(tab, ..., ...)
+        echange(tab, imin, k)
     return tab
+
+
+class TestSujet01(TestCase):
+    def test_taille(self):
+        self.assertEqual(taille(a, "F"), 9)
+        self.assertEqual(taille(a, "B"), 5)
+        self.assertEqual(taille(a, "I"), 2)
+
+    def test_echange(self):
+        a = [1, 2, 3]
+        echange(a, 0, 2)
+        self.assertEqual([3, 2, 1], a)
+
+    def test_sort(self):
+        b = [324, 1, 6, 0, 1]
+        self.assertEqual(sorted(b), tri_selection(b))
+
+    def test_sort_empty_list(self):
+        self.assertEqual([], tri_selection([]))
+
+
+if __name__ == "__main__":
+    main()
