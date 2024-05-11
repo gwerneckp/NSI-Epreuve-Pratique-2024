39fffae7f49941a01fcc111264555014110ddc50
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-11 19:37:18 +0100

    Sujet 2 completed

--- sujets/sujet_02/sujet_answer_02.py
+++ sujets/sujet_02/sujet_answer_02.py
@@ -1,28 +1,63 @@
-def correspond(a, b):
-    pass
+from unittest import TestCase, main
 
 
-def est_cyclique(plan):
+def correspond(a: str, b: str) -> bool:
+    if len(a) != len(b):
+        return False
+    for i in range(len(a)):
+        if a[i] != b[i] and b[i] != "*":
+            return False
+
+    return True
+
+
+def est_cyclique(plan: dict[str, str]) -> bool:
     """Prend en paramètre un dictionnaire `plan` correspondant à
     un plan d'envoi de messages (ici entre les personnes A, B, C,
     D, E, F).
     Renvoie True si le plan d'envoi de messages est cyclique et
     False sinon."""
     expediteur = "A"
-    destinataire = plan[...]
+    destinataire = plan[expediteur]
     nb_destinataires = 1
 
     while destinataire != expediteur:
-        destinataire = ...
-        nb_destinataires = ...
+        destinataire = plan[destinataire]
+        nb_destinataires += 1
 
-    return nb_destinataires == ...
+    return nb_destinataires == len(plan)
 
-    est_cyclique({"A": "E", "F": "A", "C": "D", "E": "B", "B": "F", "D": "C"}) == False
-)
-    est_cyclique({"A": "E", "F": "C", "C": "D", "E": "B", "B": "F", "D": "A"}) == True
-)
-    est_cyclique({"A": "B", "F": "C", "C": "D", "E": "A", "B": "F", "D": "E"}) == True
-)
-    est_cyclique({"A": "B", "F": "A", "C": "D", "E": "C", "B": "F", "D": "E"}) == False
-)
+
+class TestSujet02(TestCase):
+    def test_correspond_true(self):
+        self.assertTrue(correspond("INFORMATIQUE", "INFO*MA*IQUE"))
+
+    def test_correspond_false(self):
+        self.assertFalse(correspond("AUTOMATIQUE", "INFO*MA*IQUE"))
+
+    def test_correspond_not_same_lenght(self):
+        self.assertFalse(correspond("STOP", "S*"))
+
+    def test_cyclique_false(self):
+        self.assertFalse(
+            est_cyclique({"A": "E", "F": "A", "C": "D", "E": "B", "B": "F", "D": "C"})
+        )
+
+    def test_cyclique_false_2(self):
+        self.assertFalse(
+            est_cyclique({"A": "B", "F": "A", "C": "D", "E": "C", "B": "F", "D": "E"})
+        )
+
+    def test_cyclique_true(self):
+        self.assertTrue(
+            est_cyclique({"A": "E", "F": "C", "C": "D", "E": "B", "B": "F", "D": "A"})
+        )
+
+    def test_cyclique_true_2(self):
+        self.assertTrue(
+            est_cyclique({"A": "B", "F": "C", "C": "D", "E": "A", "B": "F", "D": "E"})
+        )
+
+
+if __name__ == "__main__":
+    main()
