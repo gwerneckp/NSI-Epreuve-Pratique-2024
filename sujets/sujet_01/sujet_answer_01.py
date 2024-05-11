8f2660e2b74f0803fd52ec940fd5eaae49deea62
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-11 22:28:41 +0100

    Add type hints and update test cases

--- sujets/sujet_01/sujet_answer_01.py
+++ sujets/sujet_01/sujet_answer_01.py
@@ -1,3 +1,4 @@
+from typing import Any
 from unittest import TestCase, main
 
 a = {
@@ -14,7 +15,7 @@ a = {
 
 
 # Votre code
-def taille(arbre: dict[str, list[str]], lettre: str):
+def taille(arbre: dict[str, list[str]], lettre: str) -> int:
     if not arbre[lettre][0] and not arbre[lettre][1]:
         return 1
     if not arbre[lettre][0]:
@@ -24,7 +25,7 @@ def taille(arbre: dict[str, list[str]], lettre: str):
     return 1 + taille(arbre, arbre[lettre][0]) + taille(arbre, arbre[lettre][1])
 
 
-def echange(tab: list, i: int, j: int) -> None:
+def echange(tab: list[Any], i: int, j: int) -> None:
     """Echange les éléments d'indice i et j dans le tableau tab."""
     # temp = tab[i]
     # tab[i] = tab[j]
@@ -46,21 +47,25 @@ def tri_selection(tab: list[int]) -> list[int]:
 
 
 class TestSujet01(TestCase):
-    def test_taille(self):
+    def test_taille_1(self) -> None:
         self.assertEqual(taille(a, "F"), 9)
+
+    def test_taille_2(self) -> None:
         self.assertEqual(taille(a, "B"), 5)
+
+    def test_taille_3(self) -> None:
         self.assertEqual(taille(a, "I"), 2)
 
-    def test_echange(self):
+    def test_echange(self) -> None:
         a = [1, 2, 3]
         echange(a, 0, 2)
         self.assertEqual([3, 2, 1], a)
 
-    def test_sort(self):
+    def test_sort(self) -> None:
         b = [324, 1, 6, 0, 1]
         self.assertEqual(sorted(b), tri_selection(b))
 
-    def test_sort_empty_list(self):
+    def test_sort_empty_list(self) -> None:
         self.assertEqual([], tri_selection([]))
 
 
