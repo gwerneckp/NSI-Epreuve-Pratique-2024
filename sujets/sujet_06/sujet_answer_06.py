9a1c63d1696c2eff909bfe53c1d86caa9aea3ab7
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-13 09:26:47 +0100

    Sujet 6 exo 2

--- sujets/sujet_06/sujet_answer_06.py
+++ sujets/sujet_06/sujet_answer_06.py
@@ -13,30 +13,27 @@ def verifie(tab: list[int]) -> bool:
     return True
 
 
-# urne = ["A", "A", "A", "B", "C", "B", "C", "B", "C", "B"]
+def depouille(urne: list[str]) -> dict[str, int]:
+    """prend en paramètre une liste de suffrages et renvoie un
+    dictionnaire avec le nombre de voix pour chaque candidat"""
+    resultat: dict[str, int] = {}
+    for bulletin in urne:
+        if resultat.get(bulletin):
+            resultat[bulletin] = resultat[bulletin] + 1
+        else:
+            resultat[bulletin] = 1
+    return resultat
 
 
-# def depouille(urne):
-#     """prend en paramètre une liste de suffrages et renvoie un
-#     dictionnaire avec le nombre de voix pour chaque candidat"""
-#     resultat = ...
-#     for bulletin in urne:
-#         if ...:
-#             resultat[bulletin] = resultat[bulletin] + 1
-#         else:
-#             ...
-#     return resultat
-
-
-# def vainqueurs(election):
-#     """prend en paramètre un dictionnaire non vide avec le nombre de voix
-#     pour chaque candidat et renvoie la liste des vainqueurs"""
-#     nmax = 0
-#     for candidat in election:
-#         if ... > ...:
-#             nmax = ...
-#     liste_finale = [nom for nom in election if ...]
-#     return ...
+def vainqueurs(election: dict[str, int]) -> list[str]:
+    """prend en paramètre un dictionnaire non vide avec le nombre de voix
+    pour chaque candidat et renvoie la liste des vainqueurs"""
+    nmax = 0
+    for candidat in election:
+        if (n := election[candidat]) > nmax:
+            nmax = n
+    liste_finale = [nom for nom in election if election[nom] == nmax]
+    return liste_finale
 
 
 class TestSujet05(TestCase):
@@ -54,6 +51,24 @@ class TestSujet05(TestCase):
 
     def test_verifie_single_element(self) -> None:
         self.assertTrue(verifie([5]))
+
+    def test_depouille_case1(self) -> None:
+        self.assertDictEqual(depouille(["A", "B", "A"]), {"A": 2, "B": 1})
+
+    def test_depouille_empty(self) -> None:
+        self.assertDictEqual(depouille([]), {})
+
+    def test_depouille_case3(self) -> None:
+        self.assertDictEqual(
+            depouille(["A", "A", "A", "B", "C", "B", "C", "B", "C", "B"]),
+            {"A": 3, "B": 4, "C": 3},
+        )
+
+    def test_vainqueurs_case1(self) -> None:
+        self.assertListEqual(vainqueurs({"A": 3, "B": 4, "C": 3}), ["B"])
+
+    def test_vainqueurs_case2(self) -> None:
+        self.assertListEqual(vainqueurs({"A": 2, "B": 2, "C": 1}), ["A", "B"])
 
 
 if __name__ == "__main__":
