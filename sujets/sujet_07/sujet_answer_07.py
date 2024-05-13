3e5b0592af90f926f90d2a952b26fd18ef8884d1
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-13 11:35:50 +0100

    change solution to the one proposed by exercice

--- sujets/sujet_07/sujet_answer_07.py
+++ sujets/sujet_07/sujet_answer_07.py
@@ -14,14 +14,6 @@ def gb_vers_entier(tab: list[bool]) -> int:
 def tri_insertion(tab: list[int]) -> list[int]:
     """Trie le tableau tab par ordre croissant
     en appliquant l'algorithme de tri par insertion"""
-    for i in range(len(tab)):
-        j = i
-        while j > 0 and tab[j] < tab[j - 1]:
-            tab[j], tab[j - 1] = tab[j - 1], tab[j]
-            j = j - 1
-
-    return tab
-
     n = len(tab)
     for i in range(1, n):
         valeur_insertion = tab[i]
@@ -30,10 +22,10 @@ def tri_insertion(tab: list[int]) -> list[int]:
         j = i
         # tant qu'on n'a pas trouvé la place de l'élément à
         # insérer on décale les valeurs du tableau vers la droite
-        while j > 0 and valeur_insertion < tab[...]:
+        while j > 0 and valeur_insertion < tab[j - 1]:
             tab[j] = tab[j - 1]
-            j = ...
-        tab[j] = ...
+            j = j - 1
+        tab[j] = valeur_insertion
     return tab
 
 
