af150e7f6807c49d8648424399a343ab06dcd009
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-13 11:31:19 +0100

    sujet 6 ex 2 (alternative answer)

--- sujets/sujet_07/sujet_answer_07.py
+++ sujets/sujet_07/sujet_answer_07.py
@@ -11,22 +11,30 @@ def gb_vers_entier(tab: list[bool]) -> int:
     return result
 
 
-# def tri_insertion(tab):
-#     """Trie le tableau tab par ordre croissant
-#     en appliquant l'algorithme de tri par insertion"""
-#     n = len(tab)
-#     for i in range(1, n):
-#         valeur_insertion = ...
-#         # la variable j sert à déterminer
-#         # où placer la valeur à ranger
-#         j = ...
-#         # tant qu'on n'a pas trouvé la place de l'élément à
-#         # insérer on décale les valeurs du tableau vers la droite
-#         while j > ... and valeur_insertion < tab[...]:
-#             tab[j] = tab[j - 1]
-#             j = ...
-#         tab[j] = ...
-#     return tab
+def tri_insertion(tab: list[int]) -> list[int]:
+    """Trie le tableau tab par ordre croissant
+    en appliquant l'algorithme de tri par insertion"""
+    for i in range(len(tab)):
+        j = i
+        while j > 0 and tab[j] < tab[j - 1]:
+            tab[j], tab[j - 1] = tab[j - 1], tab[j]
+            j = j - 1
+
+    return tab
+
+    n = len(tab)
+    for i in range(1, n):
+        valeur_insertion = tab[i]
+        # la variable j sert à déterminer
+        # où placer la valeur à ranger
+        j = i
+        # tant qu'on n'a pas trouvé la place de l'élément à
+        # insérer on décale les valeurs du tableau vers la droite
+        while j > 0 and valeur_insertion < tab[...]:
+            tab[j] = tab[j - 1]
+            j = ...
+        tab[j] = ...
+    return tab
 
 
 class TestSujet06(TestCase):
@@ -50,6 +58,11 @@ class TestSujet06(TestCase):
     def test_gb_vers_entier_case4(self) -> None:
         self.assertEqual(
             gb_vers_entier([True, False, False, False, False, False, True, False]), 130
+        )
+
+    def test_tri_insertion(self) -> None:
+        self.assertListEqual(
+            tri_insertion([98, 12, 104, 23, 131, 9]), [9, 12, 23, 98, 104, 131]
         )
 
 
