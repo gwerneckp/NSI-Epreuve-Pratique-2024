edcc7d4e179cd406c2fa202a63b29c930a778c1e
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-14 14:58:41 +0100

    sujet 23 ex 2

--- sujets/sujet_23/sujet_answer_23.py
+++ sujets/sujet_23/sujet_answer_23.py
@@ -14,21 +14,21 @@ def insertion_abr(abr: Node, cle: int) -> Node:
     return abr
 
 
-# def empaqueter(liste_masses, c):
-#     """Renvoie le nombre minimal de boîtes nécessaires pour
-#     empaqueter les objets de la liste liste_masses, sachant
-#     que chaque boîte peut contenir au maximum c kilogrammes"""
-#     n = len(liste_masses)
-#     nb_boites = 0
-#     boites = [0 for _ in range(n)]
-#     for masse in ...:
-#         i = 0
-#         while i < nb_boites and boites[i] + ... > c:
-#             i = i + 1
-#         if i == nb_boites:
-#             ...
-#         boites[i] = ...
-#     return ...
+def empaqueter(liste_masses: list[int], c: int) -> int:
+    """Renvoie le nombre minimal de boîtes nécessaires pour
+    empaqueter les objets de la liste liste_masses, sachant
+    que chaque boîte peut contenir au maximum c kilogrammes"""
+    n = len(liste_masses)
+    nb_boites = 0
+    boites = [0 for _ in range(n)]
+    for masse in liste_masses:
+        i = 0
+        while i < nb_boites and boites[i] + masse > c:
+            i = i + 1
+        if i == nb_boites:
+            nb_boites += 1
+        boites[i] = boites[i] + masse
+    return nb_boites
 
 
 class TestSujet23(TestCase):
@@ -73,6 +73,15 @@ class TestSujet23(TestCase):
             insertion_abr(self.abr1, 2),
             ((None, 0, None), 1, (None, 2, (None, 3, None))),
         )
+
+    def test_empaqueter_case_1(self) -> None:
+        self.assertEqual(empaqueter([1, 2, 3, 4, 5], 10), 2)
+
+    def test_empaqueter_case_2(self) -> None:
+        self.assertEqual(empaqueter([1, 2, 3, 4, 5], 5), 4)
+
+    def test_empaqueter_case_3(self) -> None:
+        self.assertEqual(empaqueter([7, 6, 3, 4, 8, 5, 9, 2], 11), 5)
 
 
 if __name__ == "__main__":
