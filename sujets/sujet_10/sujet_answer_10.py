8f6f8c04f3e01263449b1e74950282aa612f45ba
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-14 16:05:19 +0100

    sujet 10 ex 2

--- sujets/sujet_10/sujet_answer_10.py
+++ sujets/sujet_10/sujet_answer_10.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from unittest import TestCase, main
 
 
@@ -9,38 +10,41 @@ def moyenne(notes: list[tuple[float, float]]) -> float | None:
     return (nom / dem) if dem != 0 else None
 
 
-# def affiche(dessin):
-#     """affichage d'une grille : les 1 sont représentés par
-#     des "*" , les 0 par un espace " " """
-#     for ligne in dessin:
-#         affichage = ""
-#         for col in ligne:
-#             if col == 1:
-#                 affichage = affichage + "*"
-#             else:
-#                 affichage = affichage + " "
-#         print(affichage)
+Dessin = list[list[Literal[0, 1]]]
 
 
-# def liste_zoom(liste_depart, k):
-#     """renvoie une liste contenant k fois chaque élément de
-#     liste_depart"""
-#     liste_zoomee = ...
-#     for elt in ...:
-#         for i in range(k):
-#             ...
-#     return liste_zoomee
+def affiche(dessin: Dessin) -> None:
+    """affichage d'une grille : les 1 sont représentés par
+    des "*" , les 0 par un espace " " """
+    for ligne in dessin:
+        affichage = ""
+        for col in ligne:
+            if col == 1:
+                affichage = affichage + "*"
+            else:
+                affichage = affichage + " "
+        print(affichage)
 
 
-# def dessin_zoom(grille, k):
-#     """renvoie une grille où les lignes sont zoomées k fois
-#     ET répétées k fois"""
-#     grille_zoomee = []
-#     for ligne in grille:
-#         ligne_zoomee = ...
-#         for i in range(k):
-#             ....append(...)
-#     return grille_zoomee
+def liste_zoom(liste_depart: list, k: int) -> list:
+    """renvoie une liste contenant k fois chaque élément de
+    liste_depart"""
+    liste_zoomee = []
+    for elt in liste_depart:
+        for _ in range(k):
+            liste_zoomee.append(elt)
+    return liste_zoomee
+
+
+def dessin_zoom(grille: Dessin, k: int) -> Dessin:
+    """renvoie une grille où les lignes sont zoomées k fois
+    ET répétées k fois"""
+    grille_zoomee = []
+    for ligne in grille:
+        ligne_zoomee = liste_zoom(ligne, k)
+        for i in range(k):
+            grille_zoomee.append(ligne_zoomee)
+    return grille_zoomee
 
 
 class TestSujet10(TestCase):
@@ -54,6 +58,34 @@ class TestSujet10(TestCase):
     def test_moyenne_empty(self) -> None:
         self.assertIsNone(moyenne([(3, 0), (5, 0)]))
 
+    def test_liste_zoom(self) -> None:
+        self.assertListEqual(liste_zoom([1, 2, 3], 3), [1, 1, 1, 2, 2, 2, 3, 3, 3])
 
+    def test_grille_zoom(self) -> None:
+        self.assertListEqual(
+            dessin_zoom([[1, 0], [0, 1]], 2),
+            [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, 1]],
+        )
+
+
+print(f"From module: {__name__}")
 if __name__ == "__main__":
+    coeur: Dessin = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    affiche(coeur)
+    affiche(dessin_zoom(coeur, 2))
+
     main()
