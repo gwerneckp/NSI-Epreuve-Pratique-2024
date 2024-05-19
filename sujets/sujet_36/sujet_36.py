111f3400f50d4110509815637ff3b06cad8c1e94
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-19 19:10:02 +0100

    sujet 36

--- sujets/sujet_36/sujet_36.py
+++ sujets/sujet_36/sujet_36.py
@@ -1,18 +1,11 @@
 valeurs = [100, 50, 20, 10, 5, 2, 1]
 
+
 def rendu_glouton(a_rendre, rang):
     if a_rendre == 0:
         return ...
     v = valeurs[rang]
-    if v <= ... :
+    if v <= ...:
         return ... + rendu_glouton(a_rendre - v, rang)
-    else :
+    else:
         return rendu_glouton(a_rendre, ...)
-
->>>rendu_glouton(67, 0)
-[50, 10, 5, 2]
->>>rendu_glouton(291, 0)
-[100, 100, 50, 20, 20, 1]
->>> rendu_glouton(291,1) # si on ne dispose pas de billets de 100
-[50, 50, 50, 50, 50, 20, 20, 1]
-
