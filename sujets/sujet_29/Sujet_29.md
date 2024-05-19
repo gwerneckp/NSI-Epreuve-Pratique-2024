3ca848fefc89349a470c52c0a9bc27e6f884ef54
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-19 17:21:18 +0100

    sujet 29

--- sujets/sujet_29/Sujet_29.md
+++ sujets/sujet_29/Sujet_29.md
@@ -21,7 +21,7 @@ Dans le triangle de Pascal, chaque ligne commence et se termine par le nombre 1.
 Comme l'illustre la Figure 2, on additionne deux valeurs successives d'une ligne pour
 obtenir la valeur qui se situe sous la deuxième valeur.
 
-![image](data2023/17_triangle.png){: .center width=60%}
+![image](images/image-11.png)
 
 Compléter les fonctions `ligne_suivante` et `pascal` ci-dessous. La fonction
 `ligne_suivante` prend en paramètre une liste d'entiers `ligne` correspondant à une
@@ -29,20 +29,20 @@ ligne du triangle de Pascal et renvoie la liste correspondant à la ligne suivan
 de Pascal. La fonction `pascal` prend en paramètre un entier n et l'utilise pour construire
 le triangle de Pascal ayant `n+1` lignes sous la forme d'une liste de listes.
 
-```python 
+```python
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+    ligne_suiv = [...]
+    for i in range(...):
+        ligne_suiv.append(...)
+    ligne_suiv.append(...)
     return ligne_suiv
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
     triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+    for k in range(...):
+        ligne_k = ...
         triangle.append(ligne_k)
     return triangle
 
@@ -50,6 +50,7 @@ def pascal(n):
 ```
 
 Exemples:
+
 ```python
 >>> ligne_suivante([1, 3, 3, 1])
 [1, 4, 6, 4, 1]
--- sujets/sujet_29/sujet_answer_29.py
+++ sujets/sujet_29/sujet_answer_29.py
@@ -1,22 +1,38 @@
 from unittest import TestCase, main
 
-def ligne_suivante(ligne):
-    '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+
+def moyenne(notes: list[tuple[float, int]]) -> float:
+    total: float = 0
+    coefficient: int = 0
+    for note, coefficient in notes:
+        total += note * coefficient
+        coefficient += coefficient
+    return total / coefficient
+
+
+def ligne_suivante(ligne: list[int]) -> list[int]:
+    """Renvoie la ligne suivant ligne du triangle de Pascal"""
+    ligne_suiv = [1]
+    for i in range(len(ligne) - 1):
+        ligne_suiv.append(ligne[i + 1] + ligne[i])
+    ligne_suiv.append(1)
+
     return ligne_suiv
 
-def pascal(n):
-    '''Renvoie le triangle de Pascal de hauteur n'''
-    triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+
+def pascal(n: int) -> list[list[int]]:
+    """Renvoie le triangle de Pascal de hauteur n"""
+    triangle = [[1]]
+    for k in range(n):
+        ligne_k = ligne_suivante(triangle[k])
         triangle.append(ligne_k)
     return triangle
 
+
 class TestSujet29(TestCase):
+    def test_moyenne_case_1(self) -> None:
+        self.assertEqual(moyenne([(15.0, 2), (9.0, 1), (12.0, 3)]), 12.5)
+
     def test_ligne_suivante_case_1(self) -> None:
         self.assertEqual(ligne_suivante([1, 3, 3, 1]), [1, 4, 6, 4, 1])
 
@@ -25,6 +41,7 @@ class TestSujet29(TestCase):
 
     def test_ligne_suivante_case_3(self) -> None:
         self.assertEqual(pascal(3), [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
+
 
 if __name__ == "__main__":
     main()
