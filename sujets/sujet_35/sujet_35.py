8850827bc813761c877d168ae6c85ca2cbd866d4
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-19 19:10:52 +0100

    formatting

--- sujets/sujet_35/sujet_35.py
+++ sujets/sujet_35/sujet_35.py
@@ -1,22 +1,20 @@
-t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7]
-annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
-
 def inverse_chaine(chaine):
-    '''Retourne la chaine inversée'''
-    resultat = ... 
+    """Retourne la chaine inversée"""
+    resultat = ...
     for caractere in chaine:
-        resultat = ... 
+        resultat = ...
     return resultat
 
+
 def est_palindrome(chaine):
-    '''Renvoie un booléen indiquant si la chaine ch
-    est un palindrome'''
+    """Renvoie un booléen indiquant si la chaine ch
+    est un palindrome"""
     inverse = inverse_chaine(chaine)
-    return ... 
+    return ...
+
 
 def est_nbre_palindrome(nbre):
-    '''Renvoie un booléen indiquant si le nombre nbre 
-    est un palindrome'''
-    chaine = ... 
+    """Renvoie un booléen indiquant si le nombre nbre
+    est un palindrome"""
+    chaine = ...
     return est_palindrome(chaine)
-
