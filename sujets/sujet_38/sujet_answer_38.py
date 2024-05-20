2b469c2f581601f6963314ce5c3de5e2ef69b189
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-20 21:48:37 +0100

    sujet 38

--- sujets/sujet_38/sujet_answer_38.py
+++ sujets/sujet_38/sujet_answer_38.py
@@ -1,24 +1,41 @@
 from unittest import TestCase, main
 
-def renverse(pile):
-    '''renvoie une pile contenant les mêmes éléments que pile,
+
+def indices_maxi(tab: list[int]) -> tuple[int, list[int]]:
+    maxv = tab[0]
+    indices: list[int] = [0]
+    for i in range(1, len(tab)):
+        if tab[i] == maxv:
+            indices.append(i)
+            continue
+
+        if tab[i] > maxv:
+            maxv = tab[i]
+            indices = [i]
+
+    return (maxv, indices)
+
+
+def renverse(pile: list) -> list:
+    """renvoie une pile contenant les mêmes éléments que pile,
     mais dans l'ordre inverse.
-    Cette fonction détruit pile.'''
-    pile_inverse = ... 
+    Cette fonction détruit pile."""
+    pile_inverse = []
     while pile != []:
-        ... .append(...) 
-    return ... 
+        pile_inverse.append(pile.pop())
+    return pile_inverse
 
 
-def positifs(pile):
-    '''renvoie une pile contenant les éléments positifs de pile,
-    dans le même ordre. Cette fonction détruit pile.'''
-    pile_positifs = ... 
+def positifs(pile: list[int]) -> list[int]:
+    """renvoie une pile contenant les éléments positifs de pile,
+    dans le même ordre. Cette fonction détruit pile."""
+    pile_positifs = []
     while pile != []:
-        ... = pile.pop() 
-        if ... >= 0: 
-            ...
-    return ...
+        value = pile.pop()
+        if value >= 0:
+            pile_positifs.append(value)
+    return renverse(pile_positifs)
+
 
 class TestSujet38(TestCase):
     def test_indices_maxi_case_1(self) -> None:
@@ -27,14 +44,21 @@ class TestSujet38(TestCase):
     def test_indices_maxi_case_2(self) -> None:
         self.assertEqual(indices_maxi([7]), (7, [0]))
 
+    def test_pile_inverse_case_1(self) -> None:
+        self.assertEqual(renverse([1, 2, 3]), [3, 2, 1])
+
     def test_renverse_case_1(self) -> None:
         self.assertEqual(renverse([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+
+    def test_renverse_case_empty(self) -> None:
+        self.assertEqual(renverse([]), [])
 
     def test_renverse_case_2(self) -> None:
         self.assertEqual(positifs([-1, 0, 5, -3, 4, -6, 10, 9, -8]), [0, 5, 4, 10, 9])
 
     def test_renverse_case_3(self) -> None:
         self.assertEqual(positifs([-2]), [])
+
 
 if __name__ == "__main__":
     main()
