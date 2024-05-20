1e84f79b5b6be480e26ed51b5cc41d7628d930ec
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-20 22:10:53 +0100

    sujet 39

--- sujets/sujet_39/sujet_answer_39.py
+++ sujets/sujet_39/sujet_answer_39.py
@@ -1,51 +1,77 @@
+from typing import Optional
 from unittest import TestCase, main
 
->>> recherche(1, [2, 3, 4]) # renvoie None
->>> recherche(1, [10, 12, 1, 56])
-2
->>> recherche(1, [1, 0, 42, 7])
-0
->>> recherche(1, [1, 50, 1])
-2
->>> recherche(1, [8, 1, 10, 1, 7, 1, 8])
-5
+
+def recherche(elt: int, tab: list[int]) -> int | None:
+    last_i: int | None = None
+    for i in range(len(tab)):
+        if tab[i] == elt:
+            last_i = i
+    return last_i
+
 
 class AdresseIP:
-    def __init__(self, adresse):
-        self.adresse = ... 
+    def __init__(self, adresse: str):
+        self.adresse = adresse
 
-    def liste_octets(self):
+    def liste_octets(self) -> list[int]:
         """renvoie une liste de nombres entiers,
         la liste des octets de l'adresse IP"""
-        # Note : split découpe la chaine de caractères 
+        # Note : split découpe la chaine de caractères
         # en fonction du séparateur
         return [int(i) for i in self.adresse.split(".")]
 
-    def est_reservee(self):
+    def est_reservee(self) -> bool:
         """renvoie True si l'adresse IP est une adresse
         réservée, False sinon"""
-        reservees = [ ... ] 
-        return ... 
+        reservees = ["192.168.0.0", "192.168.0.255"]
+        return self.adresse in reservees
 
-    def adresse_suivante(self):
+    def adresse_suivante(self) -> Optional["AdresseIP"]:
         """renvoie un objet de AdresseIP avec l'adresse
         IP qui suit l'adresse self si elle existe et None sinon"""
-        octets = ... 
-        if ... == 254: 
+        octets = self.liste_octets()
+        if octets[-1] == 254:
             return None
-        octet_nouveau = ... + ... 
-        return AdresseIP('192.168.0.' + ...)
+        octet_nouveau = octets[-1] + 1
+        return AdresseIP("192.168.0." + str(octet_nouveau))
 
->>> adresse1.liste_octets()
-[192, 168, 0, 1]
->>> adresse1.est_reservee()
-False
->>> adresse3.est_reservee()
-True
->>> adresse2.adresse_suivante().adresse # acces valide à adresse
-# ici car on sait que l'adresse suivante existe
-'192.168.0.3'
 
 class TestSujet39(TestCase):
+    def test_recherche_case_none(self) -> None:
+        self.assertIsNone(recherche(1, [2, 3, 4]))
+
+    def test_recherche_case_1(self) -> None:
+        self.assertEqual(recherche(1, [10, 12, 1, 56]), 2)
+
+    def test_recherche_case_2(self) -> None:
+        self.assertEqual(recherche(1, [1, 0, 42, 7]), 0)
+
+    def test_recherche_case_3(self) -> None:
+        self.assertEqual(recherche(1, [1, 50, 1]), 2)
+
+    def test_recherche_case_4(self) -> None:
+        self.assertEqual(recherche(1, [8, 1, 10, 1, 7, 1, 8]), 5)
+
+    def setUp(self) -> None:
+        self.adresse1 = AdresseIP("192.168.0.1")
+        self.adresse2 = AdresseIP("192.168.0.2")
+        self.adresse3 = AdresseIP("192.168.0.0")
+        return super().setUp()
+
+    def test_adresse1_liste_octets_case_1(self) -> None:
+        self.assertEqual(self.adresse1.liste_octets(), [192, 168, 0, 1])
+
+    def test_adresse1_liste_octets_case_2(self) -> None:
+        self.assertFalse(self.adresse1.est_reservee())
+
+    def test_adresse1_liste_octets_case_3(self) -> None:
+        self.assertTrue(self.adresse3.est_reservee())
+
+    def test_adresse1_liste_octets_case_4(self) -> None:
+        assert (adresseSuiv := self.adresse2.adresse_suivante()) is not None
+        self.assertEqual(adresseSuiv.adresse, "192.168.0.3")
+
+
 if __name__ == "__main__":
     main()
