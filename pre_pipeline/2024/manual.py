c2578c69ab6dd079827c1e17e585dd953e3b222e
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-20 22:44:43 +0100

    used for ex 40

--- pre_pipeline/2024/manual.py
+++ pre_pipeline/2024/manual.py
@@ -1,23 +1,23 @@
 from convert import transform_repl_into_unittest
 
-first = """>>> recherche(1, [2, 3, 4])
->>> recherche(1, [10, 12, 1, 56])
-2
->>> recherche(1, [1, 0, 42, 7])
-0
->>> recherche(1, [1, 50, 1])
-2
->>> recherche(1, [8, 1, 10, 1, 7, 1, 8])
-5"""
+first = """>>> selection_enclos(animaux, 5)
+[{'nom':'Titine', 'espece':'chat', 'age':2, 'enclos':5},
+ {'nom':'Mirza', 'espece':'chat', 'age':6, 'enclos':5}]
 
-second = """>>> adresse1.liste_octets()
-[192, 168, 0, 1]
->>> adresse1.est_reservee()
-False
->>> adresse3.est_reservee()
-True
->>> adresse2.adresse_suivante().adresse
-'192.168.0.3'"""
+>>> selection_enclos(animaux, 2)
+[{'nom':'Medor', 'espece':'chien', 'age':5, 'enclos':2}]
+
+>>> selection_enclos(animaux, 7)
+[]"""
+
+second = """>>> trouver_intrus([3, 3, 3, 9, 9, 9, 1, 1, 1, 7, 2, 2, 2, 4, 4, 4, 8, 8, 8, 5, 5, 5], 0, 21)
+7
+
+>>> trouver_intrus([8, 5, 5, 5, 9, 9, 9, 18, 18, 18, 3, 3, 3], 0, 12)
+8
+
+>>> trouver_intrus([5, 5, 5, 1, 1, 1, 0, 0, 0, 6, 6, 6, 3, 8, 8, 8], 0, 15)
+3"""
 
 print(transform_repl_into_unittest(first))
 print(transform_repl_into_unittest(second))
