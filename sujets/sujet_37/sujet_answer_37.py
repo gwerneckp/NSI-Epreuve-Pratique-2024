bde9292ce80acdb4d168846c74051e0498cf31df
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-20 21:40:10 +0100

    sujet 37

--- sujets/sujet_37/sujet_answer_37.py
+++ sujets/sujet_37/sujet_answer_37.py
@@ -1,33 +1,42 @@
+from typing import Literal
 from unittest import TestCase, main
 
-def tri(tab):
-    '''tab est un tableau d'entiers contenant des 0 et des 1.
-    La fonction trie ce tableau en plaçant tous les 0 à gauche'''
-    i = ... # premier indice de la zone non triée 
-    j = ... # dernier indice de la zone non triée 
+
+def moyenne(tab: list[int]) -> float | None:
+    return sum(tab) / len(tab) if len(tab) > 0 else None
+
+
+def tri(tab: list[Literal[0, 1]]) -> None:
+    """tab est un tableau d'entiers contenant des 0 et des 1.
+    La fonction trie ce tableau en plaçant tous les 0 à gauche"""
+    i = 0  # premier indice de la zone non triée
+    j = len(tab) - 1  # dernier indice de la zone non triée
     while i < j:
         if tab[i] == 0:
-            i = ... 
+            i = i + 1
         else:
-            valeur = ... 
-            tab[j] = ... 
-            ...
-            j = ...
+            valeur = tab[j]
+            tab[j] = 1
+            tab[i] = valeur
+            j = j - 1
 
->>> tab = [0,1,0,1,0,1,0,1,0]
->>> tri(tab)
->>> tab
-[0, 0, 0, 0, 0, 1, 1, 1, 1]
 
 class TestSujet37(TestCase):
     def test_moyenne_case_1(self) -> None:
-        self.assertEqual(moyenne([5,3,8]), 5.333333333333333)
+        assert (res := moyenne([5, 3, 8])) is not None
+        self.assertAlmostEqual(res, 5.333333333333333)
 
     def test_moyenne_case_2(self) -> None:
-        self.assertEqual(moyenne([1,2,3,4,5,6,7,8,9,10]), 5.5)
+        self.assertEqual(moyenne([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 5.5)
 
     def test_moyenne_case_3(self) -> None:
-        self.assertEqual(moyenne([]), # Comportement différent suivant le traitement proposé.)
+        self.assertIsNone(moyenne([]))
+
+    def test_tri(self) -> None:
+        tab: list[Literal[0, 1]] = [0, 1, 0, 1, 0, 1, 0, 1, 0]
+        tri(tab)
+        self.assertListEqual(tab, [0, 0, 0, 0, 0, 1, 1, 1, 1])
+
 
 if __name__ == "__main__":
     main()
