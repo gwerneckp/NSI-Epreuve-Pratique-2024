2e25ff8add792947ab54ccedad45e83dc77fbb01
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-21 11:28:54 +0100

    sujet 48

--- sujets/sujet_48/sujet_answer_48.py
+++ sujets/sujet_48/sujet_answer_48.py
@@ -1,23 +1,32 @@
 from unittest import TestCase, main
 
-adj = [[1, 2], [2], [0], [0]]
 
-def nombre_suivant(s):
-    '''Renvoie le nombre suivant de celui representé par s
-    en appliquant le procédé de lecture.'''
-    resultat = ''
+def voisins_entrants(adj: list[list[int]], x: int) -> list[int]:
+    res: list[int] = []
+    for i in range(len(adj)):
+        if x in adj[i]:
+            res.append(i)
+
+    return res
+
+
+def nombre_suivant(s: str) -> str:
+    """Renvoie le nombre suivant de celui representé par s
+    en appliquant le procédé de lecture."""
+    resultat = ""
     chiffre = s[0]
     compte = 1
-    for i in range(...): 
+    for i in range(1, len(s)):
         if s[i] == chiffre:
-            compte = ... 
+            compte = compte + 1
         else:
-            resultat += ... + ... 
-            chiffre = ... 
-            ...
-    lecture_... = ... + ... 
+            resultat += str(compte) + chiffre
+            chiffre = s[i]
+            compte = 1
+    lecture_chiffre = str(compte) + chiffre
     resultat += lecture_chiffre
     return resultat
+
 
 class TestSujet48(TestCase):
     def test_voisins_entrants_case_1(self) -> None:
@@ -27,10 +36,11 @@ class TestSujet48(TestCase):
         self.assertEqual(voisins_entrants([[1, 2], [2], [0], [0]], 1), [0])
 
     def test_nombre_suivant_case_1(self) -> None:
-        self.assertEqual(nombre_suivant('1211'), '111221')
+        self.assertEqual(nombre_suivant("1211"), "111221")
 
     def test_nombre_suivant_case_2(self) -> None:
-        self.assertEqual(nombre_suivant('311'), '1321')
+        self.assertEqual(nombre_suivant("311"), "1321")
+
 
 if __name__ == "__main__":
     main()
