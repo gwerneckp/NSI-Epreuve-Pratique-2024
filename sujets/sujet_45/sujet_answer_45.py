38204f71664c239211f40b578f3d1e4d5ce22c1d
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-21 09:49:28 +0100

    sujet 45

--- sujets/sujet_45/sujet_answer_45.py
+++ sujets/sujet_45/sujet_answer_45.py
@@ -1,17 +1,31 @@
+from typing import Any
 from unittest import TestCase, main
 
-def rendu_monnaie(somme_due, somme_versee):
-    '''Renvoie la liste des pièces à rendre pour rendre la monnaie
-    lorsqu'on doit rendre somme_versee - somme_due'''
-    rendu = ... 
-    a_rendre = ... 
+
+def compte_occurrences(x: Any, tab: list) -> int:
+    count = 0
+    for v in tab:
+        if v == x:
+            count += 1
+    return count
+
+
+pieces = [1, 2, 5, 10, 20, 50, 100, 200]
+
+
+def rendu_monnaie(somme_due: int, somme_versee: int) -> list[int]:
+    """Renvoie la liste des pièces à rendre pour rendre la monnaie
+    lorsqu'on doit rendre somme_versee - somme_due"""
+    rendu = []
+    a_rendre = somme_versee - somme_due
     i = len(pieces) - 1
-    while a_rendre > ...: 
+    while a_rendre > 0:
         while pieces[i] > a_rendre:
             i = i - 1
-        rendu.append(...) 
-        a_rendre = ... 
+        rendu.append(pieces[i])
+        a_rendre = a_rendre - pieces[i]
     return rendu
+
 
 class TestSujet45(TestCase):
     def test_compte_occurrences_case_1(self) -> None:
@@ -21,13 +35,16 @@ class TestSujet45(TestCase):
         self.assertEqual(compte_occurrences(5, [-2, 3, 1, 5, 3, 7, 4]), 1)
 
     def test_compte_occurrences_case_3(self) -> None:
-        self.assertEqual(compte_occurrences('a', ['a','b','c','a','d','e','a']), 3)
+        self.assertEqual(
+            compte_occurrences("a", ["a", "b", "c", "a", "d", "e", "a"]), 3
+        )
 
     def test_rendu_monnaie_case_1(self) -> None:
         self.assertEqual(rendu_monnaie(700, 700), [])
 
     def test_rendu_monnaie_case_2(self) -> None:
         self.assertEqual(rendu_monnaie(102, 500), [200, 100, 50, 20, 20, 5, 2, 1])
+
 
 if __name__ == "__main__":
     main()
