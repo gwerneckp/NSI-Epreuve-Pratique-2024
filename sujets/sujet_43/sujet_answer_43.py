fa79e8410c2c9c4bd5b61c545d1508c3c8fb3975
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-21 08:53:59 +0100

    sujet 43

--- sujets/sujet_43/sujet_answer_43.py
+++ sujets/sujet_43/sujet_answer_43.py
@@ -1,45 +1,49 @@
 from unittest import TestCase, main
 
-[[1, 1, 1, 0, 0],
-[1, -1, 1, 1, 1],
-[2, 2, 3, 2, -1],
-[1, -1, 2, -1, 3],
-[1, 1, 2, 2, -1]]
 
-def voisinage(n, ligne, colonne):
-    """ Renvoie la liste des coordonnées des voisins de la case
-    (ligne, colonne) en gérant les cases sur les bords. """
+def a_doublon(tab: list[int]) -> bool:
+    curr = float("nan")
+    for v in tab:
+        if v == curr:
+            return True
+        curr = v
+    return False
+
+
+def voisinage(n: int, ligne: int, colonne: int) -> list[tuple[int, int]]:
+    """Renvoie la liste des coordonnées des voisins de la case
+    (ligne, colonne) en gérant les cases sur les bords."""
     voisins = []
-    for l in range(max(0,ligne-1), min(n, ligne+2)):
-        for c in range(max(0, colonne-1), min(n, colonne+2)):
-            if (l, c) != (ligne, colonne):
-                voisins.append((l,c))
+    for lig in range(max(0, ligne - 1), min(n, ligne + 2)):
+        for col in range(max(0, colonne - 1), min(n, colonne + 2)):
+            if (lig, col) != (ligne, colonne):
+                voisins.append((lig, col))
     return voisins
 
 
-def incremente_voisins(grille, ligne, colonne):
-    """ Incrémente de 1 toutes les cases voisines d'une bombe."""
-    voisins = ...
-    for l, c in voisins:
-        if grille[l][c] != ...: # si ce n'est pas une bombe
-            ...  # on ajoute 1 à sa valeur
+def incremente_voisins(grille: list[list[int]], ligne: int, colonne: int) -> None:
+    """Incrémente de 1 toutes les cases voisines d'une bombe."""
+    voisins = voisinage(len(grille), ligne, colonne)
+    for lig, col in voisins:
+        if grille[lig][col] != -1:  # si ce n'est pas une bombe
+            grille[lig][col] += 1  # on ajoute 1 à sa valeur
 
 
-
-def genere_grille(bombes):
-    """ Renvoie une grille de démineur de taille nxn où n est
+def genere_grille(bombes: list[tuple[int, int]]) -> list[list[int]]:
+    """Renvoie une grille de démineur de taille nxn où n est
     le nombre de bombes, en plaçant les bombes à l'aide de
     la liste bombes de coordonnées (tuples) passée en
-    paramètre. """
+    paramètre."""
     n = len(bombes)
     # Initialisation d'une grille nxn remplie de 0
     grille = [[0 for colonne in range(n)] for ligne in range(n)]
     # Place les bombes et calcule les valeurs des autres cases
     for ligne, colonne in bombes:
-        grille[ligne][colonne] = ... # place la bombe
-        ... # incrémente ses voisins
-    
+        grille[ligne][colonne] = -1  # place la bombe
+        incremente_voisins(grille, ligne, colonne)
+
     return grille
+
 
 class TestSujet43(TestCase):
     def test_a_doublon_case_1(self) -> None:
@@ -56,6 +60,19 @@ class TestSujet43(TestCase):
 
     def test_a_doublon_case_5(self) -> None:
         self.assertFalse(a_doublon([0, 2, 3]))
+
+    def test_genre_grille(self) -> None:
+        self.assertEqual(
+            genere_grille([(1, 1), (2, 4), (3, 1), (3, 3), (4, 4)]),
+            [
+                [1, 1, 1, 0, 0],
+                [1, -1, 1, 1, 1],
+                [2, 2, 3, 2, -1],
+                [1, -1, 2, -1, 3],
+                [1, 1, 2, 2, -1],
+            ],
+        )
+
 
 if __name__ == "__main__":
     main()
