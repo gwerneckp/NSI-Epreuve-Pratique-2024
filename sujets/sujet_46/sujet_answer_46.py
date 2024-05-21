75172fc880d73227a69e60ca15e26c3ebf790731
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-21 10:42:42 +0100

    sujet 46

--- sujets/sujet_46/sujet_answer_46.py
+++ sujets/sujet_46/sujet_answer_46.py
@@ -1,33 +1,65 @@
 from unittest import TestCase, main
 
->>> recherche([2, 3, 4, 5, 6], 5)
-3
->>> recherche([2, 3, 4, 6, 7], 5) # renvoie None
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def recherche(tab: list[int], n: int) -> int | None:
+    i, j = 0, len(tab) - 1
 
-def position_alphabet(lettre):
-    '''Renvoie la position de la lettre dans l'alphabet'''
-    return ord(lettre) - ord('A')
+    while i <= j:
+        m = (i + j) // 2
+        if tab[m] == n:
+            return m
 
-def cesar(message, decalage):
-    '''Renvoie le message codé par la méthode de César
-    pour le decalage donné'''
-    resultat = ''
-    for ... in message: 
-        if 'A' <= c and c <= 'Z':
-            indice = (...) % 26 
+        if tab[m] > n:
+            j = m - 1
+
+        i = m + 1
+
+    return None
+
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def position_alphabet(lettre: str) -> int:
+    """Renvoie la position de la lettre dans l'alphabet"""
+    return ord(lettre) - ord("A")
+
+
+def cesar(message: str, decalage: int) -> str:
+    """Renvoie le message codé par la méthode de César
+    pour le decalage donné"""
+    resultat = ""
+    for c in message:
+        if "A" <= c and c <= "Z":
+            indice = (position_alphabet(c) + decalage) % 26
             resultat = resultat + alphabet[indice]
         else:
-            resultat = ... 
+            resultat = resultat + c
     return resultat
 
+
 class TestSujet46(TestCase):
+    def test_recherche_case_1(self) -> None:
+        self.assertEqual(recherche([2, 3, 4, 5, 6], 5), 3)
+
+    def test_recherche_case_2(self) -> None:
+        self.assertEqual(recherche([1, 3, 5, 7, 9], 5), 2)
+
+    def test_recherche_case_none(self) -> None:
+        self.assertIsNone(recherche([2, 3, 4, 6, 7], 5))
+
     def test_cesar_case_1(self) -> None:
-        self.assertEqual(cesar('BONJOUR A TOUS. VIVE LA MATIERE NSI !', 4), 'FSRNSYV E XSYW. ZMZI PE QEXMIVI RWM !')
+        self.assertEqual(
+            cesar("BONJOUR A TOUS. VIVE LA MATIERE NSI !", 4),
+            "FSRNSYV E XSYW. ZMZI PE QEXMIVI RWM !",
+        )
 
     def test_cesar_case_2(self) -> None:
-        self.assertEqual(cesar('GTSOTZW F YTZX. ANAJ QF RFYNJWJ SXN !', -5), 'BONJOUR A TOUS. VIVE LA MATIERE NSI !')
+        self.assertEqual(
+            cesar("GTSOTZW F YTZX. ANAJ QF RFYNJWJ SXN !", -5),
+            "BONJOUR A TOUS. VIVE LA MATIERE NSI !",
+        )
+
 
 if __name__ == "__main__":
     main()
