c9ce067796011ea7a1a9fe05a493f8c875190b9d
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-21 10:42:50 +0100

    help for sujet 46

--- pre_pipeline/2024/manual.py
+++ pre_pipeline/2024/manual.py
@@ -1,23 +1,9 @@
 from convert import transform_repl_into_unittest
 
-first = """>>> selection_enclos(animaux, 5)
-[{'nom':'Titine', 'espece':'chat', 'age':2, 'enclos':5},
- {'nom':'Mirza', 'espece':'chat', 'age':6, 'enclos':5}]
+first = """>>> recherche([2, 3, 4, 5, 6], 5)
+3
+>>> recherche([2, 3, 4, 6, 7], 5)
+None"""
 
->>> selection_enclos(animaux, 2)
-[{'nom':'Medor', 'espece':'chien', 'age':5, 'enclos':2}]
-
->>> selection_enclos(animaux, 7)
-[]"""
-
-second = """>>> trouver_intrus([3, 3, 3, 9, 9, 9, 1, 1, 1, 7, 2, 2, 2, 4, 4, 4, 8, 8, 8, 5, 5, 5], 0, 21)
-7
-
->>> trouver_intrus([8, 5, 5, 5, 9, 9, 9, 18, 18, 18, 3, 3, 3], 0, 12)
-8
-
->>> trouver_intrus([5, 5, 5, 1, 1, 1, 0, 0, 0, 6, 6, 6, 3, 8, 8, 8], 0, 15)
-3"""
 
 print(transform_repl_into_unittest(first))
-print(transform_repl_into_unittest(second))
