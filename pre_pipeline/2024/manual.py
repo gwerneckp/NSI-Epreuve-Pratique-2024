51ec23fd2a4f6b9a7a16c645a7a2a80b710155fd
gwerneckpaiva <gwerneckpaiva@gmail.com>
2024-05-28 18:15:10 +0100

    Removed old files

--- pre_pipeline/2024/manual.py
+++
@@ -1,9 +0,0 @@
-from convert import transform_repl_into_unittest
-
-first = """>>> recherche([2, 3, 4, 5, 6], 5)
-3
->>> recherche([2, 3, 4, 6, 7], 5)
-None"""
-
-
-print(transform_repl_into_unittest(first))
